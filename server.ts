5a5e7b3628176738950805fca955b2bf10b3f3ea
jatannoury-Tradias <j.tannoury.ext@bankhaus-scheich.de>
2024-07-01 01:57:33 +0300

    feat: Adding patch book route

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+app.use(express.json());
 
 interface Book {
   id: string;
@@ -27,12 +28,12 @@ app.post('/api/books', (req, res) => {
   }
   newBook.id = uuidv4();
   books.push(newBook);
-  res.status(201).json({ message: 'Book Added' });
+  return res.status(201).json({ message: 'Book Added', data: newBook });
 });
 
 // delete book by its book ID
-app.delete('/api/books', (req, res) => {
-  const bookId: string = (req.query?.id as string) || '';
+app.delete('/api/books/:id', (req, res) => {
+  const { id: bookId } = req.params;
   if (!bookId) {
     return res.status(422).json({ message: 'A book Id is required!' });
   }
@@ -44,6 +45,31 @@ app.delete('/api/books', (req, res) => {
   }
 
   return res.status(200).json({ message: 'Book deleted successfully' });
+});
+
+app.patch('/api/books/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, author } = req.body;
+
+  if (!id) {
+    return res.status(422).json({ message: 'A book Id is required!' });
+  }
+  if (!title && !author) {
+    return res.status(422).json({ message: 'No updatable field provided!' });
+  }
+
+  let bookIndex = books.findIndex((book) => book.id === id);
+  if (bookIndex === -1) {
+    return res.status(404).json({ message: 'Book not found!' });
+  }
+
+  if (title) books[bookIndex].title = title;
+  if (author) books[bookIndex].author = author;
+
+  return res.status(200).json({
+    message: 'Book updated successfully!',
+    data: books[bookIndex],
+  });
 });
 
 const server = app.listen(3030, () => {
